8679e867c01ce09d873ef8eee753038833d0007a
s6704062611255 <s6704062611255@email.kmutnb.ac.th>
2026-02-11 18:04:44 +0700

    show all score

---
+++ lab8no2.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+int checkscore(char std[]);
+
+int main()
+{
+    int i, j;
+    char ans[8][10] = {
+        {'A', 'B', 'A', 'C', 'C', 'D', 'E', 'E', 'A', 'D'},  // 7
+        {'D', 'B', 'A', 'B', 'C', 'A', 'E', 'E', 'A', 'D'},  // 6
+        {'E', 'D', 'D', 'A', 'C', 'B', 'E', 'E', 'A', 'D'},  // 5
+        {'C', 'B', 'A', 'E', 'D', 'C', 'E', 'E', 'A', 'D'},  // 4
+        {'A', 'B', 'D', 'C', 'C', 'D', 'E', 'E', 'A', 'D'},  // 8
+        {'B', 'B', 'E', 'C', 'C', 'D', 'E', 'E', 'A', 'D'},  // 7
+        {'B', 'B', 'A', 'C', 'C', 'D', 'E', 'E', 'A', 'D'},  // 7
+        {'E', 'B', 'E', 'C', 'C', 'D', 'E', 'E', 'A', 'D'}}; // 7
+
+    char keys[10] = {'D', 'B', 'D', 'C', 'C', 'D', 'A', 'E', 'A', 'D'};
+    int size = sizeof(ans) / sizeof(ans[0]);
+
+    for (int i = 0; i < size;i++){
+         printf("std %d => %d\n", (i + 1), checkscore(ans[i]));
+    }
+    
+    
+}
+int checkscore(char std[]){
+    int i ,score = 0;
+    char keys[10] = {'D', 'B', 'D', 'C', 'C', 'D', 'A', 'E', 'A', 'D'};
+
+    for(i=0 ;i<10;i++){
+        if(std[i] == keys[i]){
+            score+=1;
+        }
+    }
+    return score;
+}
