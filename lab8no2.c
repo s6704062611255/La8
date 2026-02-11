b05d395fae3b9dc3c79f5402d921769557af2b9c
s6704062611255 <s6704062611255@email.kmutnb.ac.th>
2026-02-11 18:29:40 +0700

    show number of no.1 stat

--- lab8no2.c
+++ lab8no2.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
 int checkscore(char std[]);
+int Check_No1(char std[]);
 
-int main()
-{
-    int i, j;
+int main(){
+   
     char ans[8][10] = {
         {'A', 'B', 'A', 'C', 'C', 'D', 'E', 'E', 'A', 'D'},  // 7
         {'D', 'B', 'A', 'B', 'C', 'A', 'E', 'E', 'A', 'D'},  // 6
@@ -15,13 +15,12 @@ int main()
         {'E', 'B', 'E', 'C', 'C', 'D', 'E', 'E', 'A', 'D'}}; // 7
 
     char keys[10] = {'D', 'B', 'D', 'C', 'C', 'D', 'A', 'E', 'A', 'D'};
+    
     int size = sizeof(ans) / sizeof(ans[0]);
-
     for (int i = 0; i < size;i++){
          printf("std %d => %d\n", (i + 1), checkscore(ans[i]));
     }
-    
-    
+    printf("No1 Correct: %d \n", Check_No1(ans));
 }
 int checkscore(char std[]){
     int i ,score = 0;
@@ -33,4 +32,16 @@ int checkscore(char std[]){
         }
     }
     return score;
+}
+
+int Check_No1(char std[]){
+    int score_no1 = 0;
+    char keys[10] = {'D', 'B', 'D', 'C', 'C', 'D', 'A', 'E', 'A', 'D'};
+    for(int i=0;i<8;i++){
+        
+        if(std[i] == keys[0]){
+            score_no1++;
+        }    
+    }
+    return score_no1;
 }
